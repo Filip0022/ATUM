2ae7fdf5a6aeec5e17906837dce22267f449d2e6
Filip Klos <fklosik00@icloud.com>
2023-07-27 20:51:43 +0200

    Add gallery without center

--- src/containers/gallery/Gallery.jsx
+++ src/containers/gallery/Gallery.jsx
@@ -1,20 +1,56 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react';
+import { render } from 'react-dom';
+import ImageViewer from 'react-simple-image-viewer';
 import './Gallery.css';
 
-const Gallery = () => {
-  return (
-    <section id="gallery">
-       <img src="/images/gallery1.JPG"/>
-       <img src="/images/gallery1.JPG"/>
-       <img src="/images/gallery1.JPG"/>
-       <img src="/images/gallery1.JPG"/>
-       <img src="/images/gallery1.JPG"/>
-       <img src="/images/gallery1.JPG"/>
-       <img src="/images/gallery1.JPG"/>
-       <img src="/images/gallery1.JPG"/>
-       <img src="/images/gallery1.JPG"/>
-    </section>
-  )
+
+function Gallery() {
+    const [currentImage, setCurrentImage] = useState(0);
+    const [isViewerOpen, setIsViewerOpen] = useState(false);
+    const images = [
+        'https://zoonews.pl/wp-content/uploads/2022/02/male-kotki.jpg',
+        'https://zoonews.pl/wp-content/uploads/2022/02/male-kotki.jpg',
+        'https://zoonews.pl/wp-content/uploads/2022/02/male-kotki.jpg',
+        'https://zoonews.pl/wp-content/uploads/2022/02/male-kotki.jpg',
+        'https://zoonews.pl/wp-content/uploads/2022/02/male-kotki.jpg',
+        'https://zoonews.pl/wp-content/uploads/2022/02/male-kotki.jpg',
+        'https://zoonews.pl/wp-content/uploads/2022/02/male-kotki.jpg',
+    ];
+
+    const openImageViewer = useCallback((index) => {
+        setCurrentImage(index);
+        setIsViewerOpen(true);
+    }, []);
+
+    const closeImageViewer = () => {
+        setCurrentImage(0);
+        setIsViewerOpen(false);
+    };
+
+    return (
+        <div id="gallery">
+            {images.map((src, index) => (
+                <img
+                    src={ src }
+                    onClick={ () => openImageViewer(index) }
+                    width="300"
+                    key={ index }
+                    style={{ margin: '1rem' }}
+                    alt=""
+                />
+            ))}
+
+            {isViewerOpen && (
+                <ImageViewer
+                    src={ images }
+                    currentIndex={ currentImage }
+                    disableScroll={ false }
+                    closeOnClickOutside={ true }
+                    onClose={ closeImageViewer }
+                />
+            )}
+        </div>
+    );
 }
 
 export default Gallery
